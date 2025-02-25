f5d16494646e7069d3ae22decf9e453206ae1c70
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-02-25 16:16:49 -0700

    created a shared form component

---
+++ src/app/supporting-components/personal-details-form/personal-details-form.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-personal-details-form',
+  standalone: true,
+  providers:[provideNativeDateAdapter()],
+  imports: [    
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  templateUrl: './personal-details-form.component.html',
+  styleUrl: './personal-details-form.component.css'
+})
+export class PersonalDetailsFormComponent {
+  @Input() form!:FormGroup;
+  genders:any[]=[
+      {value:'male', viewValue:'Male'},
+      {value:'female', viewValue:'Female'},
+      {value:'transgender', viewValue:'Transgender'}
+    ]
+  getFormValues(){
+    const formValues = {
+      first_name: this.form.value.first_name,
+      last_name: this.form.value.last_name,
+      email: this.form.value.email,
+      gender: this.form.value.gender,
+      phone: this.form.value.phone,
+      dob: this.form.value.dob,
+    }
+    return formValues
+  }
+}
