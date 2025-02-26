88387550442067c7983bf6c0219885dd8eeb3abb
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-02-25 20:04:22 -0700

    Basic functionality of add-record component completed with some styling

---
+++ src/app/pages/add-record/add-record.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { PersonalDetailsFormComponent } from "../../supporting-components/personal-details-form/personal-details-form.component";
+import { OverlayService } from '../../services/overlay.service';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { ContentService } from '../../services/content.service';
+import { CommunicationService } from '../../services/communication.service';
+
+@Component({
+  selector: 'app-add-record',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    PersonalDetailsFormComponent,
+    CommonModule,
+    MatButtonModule
+],
+  templateUrl: './add-record.component.html',
+  styleUrl: './add-record.component.css',
+})
+export class AddRecordComponent {
+  commsService = inject(CommunicationService)
+  contentService = inject(ContentService)
+  @ViewChild(PersonalDetailsFormComponent) formBodyComponent!: PersonalDetailsFormComponent;
+  form:FormGroup = new FormGroup({
+    first_name: new FormControl('',[Validators.required]),
+    last_name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    gender: new FormControl<string|null>(null,[Validators.required]),
+    phone: new FormControl('',[]),
+    dob: new FormControl<Date|null>(null,[Validators.required])
+  }) 
+  overlayService = inject(OverlayService)
+  async onSubmit(){
+    const response = await this.contentService.createRecord(this.formBodyComponent.getFormValues())
+    console.log(response)
+    this.commsService.changeChildData("UPDATE GRID")
+    this.overlayService.close()
+  }
+  onClose(){
+    this.overlayService.close()
+  }
+}
