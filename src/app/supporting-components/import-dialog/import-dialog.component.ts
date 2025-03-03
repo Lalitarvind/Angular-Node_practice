9651b0b4a4eeab4d541987e176b6ff49a3e9ad93
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-03-02 18:30:45 -0700

    Created basic functionality of import-dialog component

---
+++ src/app/supporting-components/import-dialog/import-dialog.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { OverlayService } from '../../services/overlay.service';
+import { MatInputModule } from '@angular/material/input';
+import { ContentService } from '../../services/content.service';
+
+@Component({
+  selector: 'app-import-dialog',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule,MatButtonModule,MatInputModule],
+  templateUrl: './import-dialog.component.html',
+  styleUrl: './import-dialog.component.css'
+})
+export class ImportDialogComponent {
+  overlayService = inject(OverlayService)
+  records!:any[]
+  selectedFile!:File;
+  contentService = inject(ContentService)
+
+
+  form:FormGroup = new FormGroup({
+    file_select: new FormControl('',[Validators.required])
+  })
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+  async onSubmit(){
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    let response = await this.contentService.importRecords(formData)
+    console.log(response)
+  }
+  onClose(){
+    this.overlayService.close();
+  }
+}
