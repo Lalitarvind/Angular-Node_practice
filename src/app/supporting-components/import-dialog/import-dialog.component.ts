f294194708c533f1e0f5923f70c5e80669ade825
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-03-07 18:01:25 -0700

    small changes in import and export dialog component

--- src/app/supporting-components/import-dialog/import-dialog.component.ts
+++ src/app/supporting-components/import-dialog/import-dialog.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { OverlayService } from '../../services/overlay.service';
 import { MatInputModule } from '@angular/material/input';
 import { ContentService } from '../../services/content.service';
+import { CommunicationService } from '../../services/communication.service';
 
 @Component({
   selector: 'app-import-dialog',
@@ -18,7 +19,7 @@ export class ImportDialogComponent {
   records!:any[]
   selectedFile!:File;
   contentService = inject(ContentService)
-
+  commsService = inject(CommunicationService)
 
   form:FormGroup = new FormGroup({
     file_select: new FormControl('',[Validators.required])
@@ -33,6 +34,8 @@ export class ImportDialogComponent {
 
     let response = await this.contentService.importRecords(formData)
     console.log(response)
+    this.commsService.changeChildData("UPDATE GRID")
+    this.onClose()
   }
   onClose(){
     this.overlayService.close();
